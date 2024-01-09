98c4e53989045c8fb95a6c64987eae10a31b65e4
Badal Chhipa <badalchhipa9@gmail.com>
2024-01-09 11:44:35 +0530

    Responsiveness completed => Have to check header__down_box

--- script.js
+++ script.js
@@ -7,6 +7,15 @@ const countriesListContainer = document.querySelector(".list__country");
 const countryArrow = document.querySelector(".country__arrow_icon");
 const nevbar = document.querySelector(".header__down");
 const nevbarTop = document.querySelector(".header__top");
+const navBarParent = document.querySelector(".header__down_box");
+const content = document.querySelector(".details__text");
+const showMoreBtn = document.querySelector(".show__more_btn");
+const showLessBtn = document.querySelector(".show__less_btn");
+
+//TODO: Initial State
+if (content.scrollHeight > content.clientHeight) {
+  showMoreBtn.style.display = "block";
+}
 
 //TODO: Checking if respective component have hidden class or not
 const checkHidden = function (component, arrow) {
@@ -61,9 +70,13 @@ countrySelectionBox.addEventListener("click", () => {
 //TODO:  Sticky Navigation
 const observerCallback = (entries) => {
   const [entry] = entries;
-  !entry.isIntersecting
-    ? nevbar.classList.add("fixed")
-    : nevbar.classList.remove("fixed");
+  if (!entry.isIntersecting) {
+    nevbar.classList.add("fixed");
+    navBarParent.style.height = "75px";
+  } else {
+    nevbar.classList.remove("fixed");
+    navBarParent.style.height = "auto";
+  }
 };
 
 const observerOptions = {
@@ -73,3 +86,16 @@ const observerOptions = {
 
 const observer = new IntersectionObserver(observerCallback, observerOptions);
 observer.observe(nevbarTop);
+
+//TODO: Show and hide text
+function toggleContent() {
+  if (content.style.maxHeight) {
+    content.style.maxHeight = null;
+    showMoreBtn.style.display = "block";
+    showLessBtn.style.display = "none";
+  } else {
+    content.style.maxHeight = "100%";
+    showMoreBtn.style.display = "none";
+    showLessBtn.style.display = "block";
+  }
+}
